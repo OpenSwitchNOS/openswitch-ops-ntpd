dbd081e6f16a65bfa7714e38d63059861efe7a70
Nilesh S Shinde <nilesh.shinde@hpe.com>
2016-05-12 16:19:39 -0700

    Limit remote to 15 and name/ref id to 39 char in show ntp associations
    With this commit, Remote is limited to 15 characters and
    name/ref-id is limited to 39 cahrs in the "show ntp
    associations" command
    
    Tags: chg, usr
    
    Change-Id: I7442ad4f3aa143dc19347a8cfc75068c2ebbddbd
    Signed-off-by: Nilesh S Shinde <nilesh.shinde@hpe.com>

--- src/cli/ntp_vty.c
+++ src/cli/ntp_vty.c
@@ -549,14 +549,17 @@ vtysh_ovsdb_show_ntp_associations()
     const struct ovsrec_ntp_association *ntp_assoc_row = NULL;
     int i = 0;
     const char *buf = NULL;
-    char dest[16];
+    char dest[39];
+    char dest_rem[16];
 
-    vty_out(vty, "----------------------------------------------------------------------------------------------------------------------\n");
-    vty_out(vty, " %3s  %15s  %15s  %3s  %5s",
+    vty_out(vty, "------------------------------------------------------------------------------------"
+                 "-----------------------------------------------------------------------------------\n");
+    vty_out(vty, " %3s  %39s  %15s  %3s  %5s",
         "ID", "NAME", "REMOTE", "VER", "KEYID");
-    vty_out(vty, "  %15s  %2s  %1s  %4s  %4s  %5s  %7s  %6s  %6s\n",
+    vty_out(vty, "  %39s  %2s  %1s  %4s  %4s  %5s  %7s  %6s  %6s\n",
         "REF-ID", "ST", "T", "LAST", "POLL", "REACH", "DELAY", "OFFSET", "JITTER");
-    vty_out(vty, "----------------------------------------------------------------------------------------------------------------------\n");
+    vty_out(vty, "-------------------------------------------------------------------------------------"
+                 "----------------------------------------------------------------------------------\n");
 
     OVSREC_NTP_ASSOCIATION_FOR_EACH(ntp_assoc_row, idl) {
         buf = smap_get(&ntp_assoc_row->association_status, NTP_ASSOC_STATUS_PEER_STATUS_WORD);
@@ -589,11 +592,12 @@ vtysh_ovsdb_show_ntp_associations()
         vty_out(vty, "%3d", ++i);
 
         snprintf(dest, sizeof(dest), "%s", ntp_assoc_row->address);
-        vty_out(vty, "  %15s", dest);
+        vty_out(vty, "  %39s", dest);
 
 
         buf = smap_get(&ntp_assoc_row->association_status, NTP_ASSOC_STATUS_REMOTE_PEER_ADDRESS);
-        vty_out(vty, "  %15s", ((buf) ? buf : ""));
+        snprintf(dest_rem, sizeof(dest_rem), "%s", buf);
+        vty_out(vty, "  %15s", dest_rem);
 
         buf = smap_get(&ntp_assoc_row->association_attributes, NTP_ASSOC_ATTRIB_VERSION);
         vty_out(vty, "  %3s", ((buf) ? buf : ""));
@@ -605,7 +609,8 @@ vtysh_ovsdb_show_ntp_associations()
         }
 
         buf = smap_get(&ntp_assoc_row->association_status, NTP_ASSOC_STATUS_REMOTE_PEER_REF_ID);
-        vty_out(vty, "  %15s", ((buf) ? buf : ""));
+        snprintf(dest, sizeof(dest), "%s", buf);
+        vty_out(vty, "  %39s", dest);
 
         buf = smap_get(&ntp_assoc_row->association_status, NTP_ASSOC_STATUS_STRATUM);
         vty_out(vty, "  %2s", ((buf) ? buf : ""));
@@ -654,7 +659,8 @@ vtysh_ovsdb_show_ntp_associations()
         vty_out(vty, "\n");
     }
 
-    vty_out(vty, "----------------------------------------------------------------------------------------------------------------------\n");
+    vty_out(vty, "------------------------------------------------------------------------------------"
+                 "-----------------------------------------------------------------------------------\n");
 }
 
 static void
